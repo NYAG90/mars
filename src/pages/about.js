ffde8837ee7f18c5ec99a91d6861290299bd395b
Nana Yaw <Adugyamfi.nyag@gmail.com>
2022-10-26 11:01:40 +0000

    First change.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const About = () => {
-return (
-	<div>
-	<h1>
-		We came from far ,near and wide.
-	</h1>
-	</div>
-);
+	return (
+		<div>
+			<h1>
+				We came from far ,near and wide.We not turning back.
+			</h1>
+		</div>
+	);
 };
 
 export default About;
--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Blogs = () => {
-return (
-	<h1>You can write your blogs!</h1>
-);
+	return (
+		<h1>You can write your blogs!Ameyaw Debrah lives here.</h1>
+	);
 };
 
 export default Blogs;
